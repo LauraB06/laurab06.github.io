c9397290cdae7e942b213f55d276b0c88cb4462a
Laura Oliveira <lauralbbdo18@gmail.com>
2020-06-13 04:07:44 -0300

    Initial commit

---
+++ src/components/Header/components/NavigationBar/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './index.scss';
+
+const NavigationBar = () => {
+  return (
+    <nav className='NavigationBar'>
+      <ul>
+        <li>
+          <NavLink exact to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/works">Works</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavigationBar;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavigationBar from './components/NavigationBar'
+
+const Header = () => {
+  return (
+    <header>
+      <NavigationBar />
+    </header>
+  );
+}
+
+export default Header;
