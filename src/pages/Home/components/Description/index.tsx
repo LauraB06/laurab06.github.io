07ad197892b6c375a6a230a6604cffd9ebb1eda7
Laura Oliveira <lauralbbdo18@gmail.com>
2020-06-13 23:50:48 -0300

    Add Description to Home

---
+++ src/pages/Home/components/Description/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './index.scss';
+
+const Description = () => {
+  return (
+    <section className="description">
+      <h1>Laura Oliveira</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae felis pharetra, blandit eros eu, tincidunt ipsum. Praesent quis massa gravida, aliquet urna non, pulvinar elit. Nulla pulvinar tincidunt lorem, sit amet aliquet enim. Integer malesuada dui mauris, non congue tellus sollicitudin in.</p>
+    </section>
+  );
+}
+
+export default Description;
